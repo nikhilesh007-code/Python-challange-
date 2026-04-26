39b3be2ecbbd170935f8734607c8d3f03920192a
nikhilesh007-code <nikhilesh.nanimelakasthuri@gmail.com>
2026-04-26 21:19:25 +0530

    Update day-9 Smart Inventory Mutation Tracker.py

--- day-9 Smart Inventory Mutation Tracker.py
+++ day-9 Smart Inventory Mutation Tracker.py
@@ -8,41 +8,50 @@ def get_data():
 
 def update_items(arr, r):
     pos = r % len(arr)
+
     for i in range(len(arr)):
         if i == pos:
             arr[i]["details"]["price"] = int(arr[i]["details"]["price"] * 0.9)
             arr[i]["details"]["stock"] += 5
+
     return arr
 
 def check_diff(a, b):
     changed = []
     unchanged = []
+
     for i in range(len(a)):
         if a[i] == b[i]:
             unchanged.append(a[i]["item"])
         else:
             changed.append(a[i]["item"])
+
     return changed, unchanged
 
+# Personalized Roll Number
 roll = 24110011589
 
 main_data = get_data()
 
-copy1 = main_data.copy()
-copy2 = copy.deepcopy(main_data)
+# Copies
+copy1 = main_data.copy()          # Shallow Copy
+copy2 = copy.deepcopy(main_data) # Deep Copy
 
+# Update
 update_items(copy1, roll)
 update_items(copy2, roll)
 
-print(main_data)
-print(copy1)
-print(copy2)
+# Print Results
+print("Original:", main_data)
+print("Shallow Copy:", copy1)
+print("Deep Copy:", copy2)
 
+# Comparison
 c1, u1 = check_diff(main_data, copy1)
 c2, u2 = check_diff(main_data, copy2)
 
-print(c1, u1)
-print(c2, u2)
+print("Shallow Changed:", c1, "Unchanged:", u1)
+print("Deep Changed:", c2, "Unchanged:", u2)
 
-print((len(c1), len(u1)))
-print((len(c2), len(u2)))
+print("Shallow Count:", (len(c1), len(u1)))
+print("Deep Count:", (len(c2), len(u2)))
